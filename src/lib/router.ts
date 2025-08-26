26b5c69d4c3952f4033eb2e46c1af58ab49fe219
alijundev <alijun.dev@gmail.com>
2025-08-27 06:32:41 +0800

    remove unused comment

--- src/lib/router.ts
+++ src/lib/router.ts
@@ -1,41 +1,37 @@
 import { writable, derived } from "svelte/store";
 
-import type {Component} from 'svelte'
+import type { Component } from "svelte";
 export const currentRoute = writable(window.location.pathname);
 
 interface Route {
   path: string;
   component: Component;
 }
-// daftar routes
+
 export let routes: Route[] = [];
 let Fallback: Component;
 
-// setup router
-export function createRouter(routeTable: Route[], fallback: Component) {
+export function createRouter(routeTable: Route[], fallback?: Component) {
   routes = routeTable;
-  Fallback = fallback;
+  Fallback = fallback || routes.find((route) => route.path === "/")!.component;
 
-  // listen back/forward
   window.addEventListener("popstate", () => {
     console.log(window.location.pathname);
     currentRoute.set(window.location.pathname);
-    
   });
 
   return {
-    push(path) {
+    push(path: string) {
       window.history.pushState({}, "", path);
       currentRoute.set(path);
     },
-    replace(path) {
+    replace(path: string) {
       window.history.replaceState({}, "", path);
       currentRoute.set(path);
     },
   };
 }
 
-// cari komponen berdasarkan route
 export const activeComponent = derived(currentRoute, ($path) => {
   const found = routes.find((r) => r.path === $path);
   return found ? found.component : Fallback;
